7b8c51e11705ab2e636e9bd945ecec3621db190c
banutu98 <serbanstol@yahoo.com>
2019-10-29 00:45:54 +0200

    readded removed file

---
+++ src/main/java/data/dao/jdbc/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package data.dao.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection conn;
+
+    public static DatabaseConnection getInstance() throws ClassNotFoundException, SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private DatabaseConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        final String URL = "jdbc:mysql://localhost:3306/mysql";
+        final String USER = "localhost";
+        final String PASS = "";
+        conn = DriverManager.getConnection(URL, USER, PASS);
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+}
